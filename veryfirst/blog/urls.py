618d52b2a6b2b2e0481773e1e2c2f9e1ae4e5f55
Александр Агакин <agakinalexandr@gmail.com>
2023-03-09 01:13:57 +0100

    blog - detail page

--- veryfirst/blog/urls.py
+++ veryfirst/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogView
+from .views import BlogView, BlogDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', BlogView.as_view(), name='list')
+    path('', BlogView.as_view(), name='list'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail')
 ]
--- veryfirst/blog/views.py
+++ veryfirst/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
@@ -6,3 +6,8 @@ from blog.models import Post
 class BlogView(ListView):
     model = Post
     template_name = 'blog/home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
