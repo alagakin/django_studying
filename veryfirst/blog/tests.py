4a717d79d56e5ff8ccac4bfa48ea8f978191f35a
Александр Агакин <agakinalexandr@gmail.com>
2023-03-09 08:28:14 +0100

    tests for deleting, updating, creating and absolute path

--- veryfirst/blog/tests.py
+++ veryfirst/blog/tests.py
@@ -28,7 +28,7 @@ class BlogTests(TestCase):
         self.assertEqual(f'{self.post.body}', 'Nice body content')
         self.assertEqual(f'{self.post.author}', 'testuser')
 
-    def test_post_lost_view(self):
+    def test_post_list_view(self):
         response = self.client.get(reverse('blog:list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'A good title')
@@ -41,3 +41,27 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'blog/post_detail.html')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/blog/post/1/')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('blog:post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user.id
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New title')
+        self.assertEqual(Post.objects.last().body, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('blog:post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text'
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('blog:post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
